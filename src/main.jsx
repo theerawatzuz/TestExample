e8c59a65438c4e93dc72649860dca507247a40bc
TheerawatNB <bankt21@gmail.com>
2024-02-03 16:20:33 +0700

    ChangeTheme

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Report from './pages/Report.jsx'
+import theme from './themes/theme.jsx'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Report />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Report />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
